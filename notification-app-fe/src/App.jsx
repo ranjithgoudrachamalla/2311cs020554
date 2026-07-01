91b7299df9bf2dd7431e44d9925376c3d4129dab
mrgoudsaab <ranjithrachamalla2.com>
2026-07-01 13:27:24 +0530

    Updated code

--- notification-app-fe/src/App.jsx
+++ notification-app-fe/src/App.jsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react";
+import { Log } from "./api/logger";
+import { getNotifications } from "./api/notifications";
+
 export default function App() {
-  return "Notifications App";
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    async function loadNotifications() {
+      try {
+        const data = await getNotifications();
+
+        setNotifications(data.notifications || []);
+
+        Log(
+          "frontend",
+          "info",
+          "api",
+          "Notifications fetched successfully"
+        );
+      } catch (error) {
+        Log(
+          "frontend",
+          "error",
+          "api",
+          error.message
+        );
+      }
+    }
+
+    loadNotifications();
+  }, []);
+
+  return (
+    <div>
+      <h1>Notifications App</h1>
+
+      <p>Total Notifications: {notifications.length}</p>
+    </div>
+  );
 }
--- notification-app-fe/src/api/logger.js
+++ notification-app-fe/src/api/logger.js
@@ -1,18 +1,26 @@
-
 const BASE_URL = "http://4.224.186.213/evaluation-service";
 
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJyYW5qaXRocmFjaGFtYWxsYTJAZ21haWwuY29tIiwiZXhwIjoxNzgyODg1NzE0LCJpYXQiOjE3ODI4ODQ4MTQsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiI3ODkyMmJhYi0zOTQxLTQ5ZTMtOWEyYy1kOTk3ZTE2MDU0YWEiLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJyYWNoYW1hbGxhIHJhbmppdGhnb3VkIiwic3ViIjoiYTdmMjQ0M2UtMTE2Mi00NzllLTk1ZmYtNmUxMTYxMDI5Y2JlIn0sImVtYWlsIjoicmFuaml0aHJhY2hhbWFsbGEyQGdtYWlsLmNvbSIsIm5hbWUiOiJyYWNoYW1hbGxhIHJhbmppdGhnb3VkIiwicm9sbE5vIjoiMjMxMWNzMDIwNTU0IiwiYWNjZXNzQ29kZSI6InhwUWRkZCIsImNsaWVudElEIjoiYTdmMjQ0M2UtMTE2Mi00NzllLTk1ZmYtNmUxMTYxMDI5Y2JlIiwiY2xpZW50U2VjcmV0IjoiRVBQUktEc3V2U1FaaEhORCJ9.xlqAW2EvCCEtO2cGreRIXrbAlz1uu9I2ypsLs7-G1tE";
 
-export async function getNotifications() {
-  const response = await fetch(`${BASE_URL}/notifications`, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
+export async function Log(stack, level, packageName, message) {
+  try {
+    const response = await fetch(`${BASE_URL}/logs`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${TOKEN}`,
+      },
+      body: JSON.stringify({
+        stack,
+        level,
+        package: packageName,
+        message,
+      }),
+    });
 
-  if (!response.ok) {
-    throw new Error("Unable to fetch notifications");
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
   }
-
-  return await response.json();
 }
